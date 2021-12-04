aad93fecdcd2bf06dad360e0c8a37d66755b637d
Rohil Gupta <rohilvg@gmail.com>
2021-12-03 20:53:22 -0500

    day2 part b

--- day2b.py
+++ day2b.py
@@ -1,16 +1,17 @@
 f = open("input.txt", "r")
 dat = f.read().splitlines()
 
-x,y = 0,0
+x,y,aim = 0,0,0
 
 for command in dat:
     course, val = command.split(" ")
     val = int(val)
     if course == "forward":
-        x += val
+       x += val
+       y += aim*val
     elif course == "down":
-        y += val
+        aim += val
     elif course == "up":
-        y -= val
+        aim -= val
 
 print(x,y,x*y)
