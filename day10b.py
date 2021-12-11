1ea8f89bdd235d38ec6c37d273440e38ad6be80b
Rohil Gupta <rohilvg@gmail.com>
2021-12-11 01:33:34 -0500

    day10b

--- day10b.py
+++ day10b.py
@@ -2,21 +2,32 @@ f = open("input.txt", "r")
 #f = open("testInput.txt", "r")
 dat = f.read().splitlines()
 
-s = []
-m = {"]":"[", "}":"{", '>': '<', ")": "("}
-p = {"]":57, "}":1197, '>': 25137, ")": 3}
+m = {"]":"[", "}":"{", ">": "<", ")": "("}
+p = {"[":2, "{":3, "<": 4, "(": 1}
 
-points = 0
+pArr =  []
 for line in dat:
+    points = 0
+    corrupt = False
+    s = []
+    
     for c in line:
         if c in "([{<":
             s.append(c)
         elif s[-1] != m[c]:
-            points += p[c]
+            corrupt = True
             break
         else:
             s.pop()
-    
 
-print(points)
+    if not corrupt:
+        while s != []:
+            points *= 5
+            points += p[s.pop()]
+
+        pArr.append(points)
+    
+pArr.sort()
+
+print(pArr[len(pArr)//2])
 
